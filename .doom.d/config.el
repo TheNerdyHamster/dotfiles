e43d8e9de2dbbb24b99b4526d35a11c16e3d2310
TheNerdyHamster <leo@letnh.com>
2020-10-05 23:17:16 +0200

    Added .net support and other stuff

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,3 +1,34 @@
+(getenv "PATH")
+
+(setq user-full-name "Leo Rönnebro"
+      user-mail-address "leo.ronnebro@hamsterapps.net")
+
+(eval-after-load
+  'company
+  '(add-to-list 'company-backends #'company-omnisharp))
+
+(defun custom-csharp-mode-setup ()
+  (omnisharp-mode)
+  (company-mode)
+  (flycheck-mode)
+
+  (setq indent-tabs-mode nil)
+  (setq c-syntactic-indentation t)
+  (c-set-style "ellemtel")
+  (setq c-basic-offset 4)
+  (setq truncate-lines t)
+  (setq tab-width 4)
+  (setq evil-shift-width 4)
+
+  (electric-pair-local-mode 1) ;; Emacs 25
+
+  (local-set-key (kbd "C-c R") 'dotnet-run)
+  (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
+  (local-set-key (kdb "C-c f") 'omnisharp-code-format-entire-file)
+  (local-set-key (kbd "C-c C-c") 'recompile))
+
+(add-hook 'csharp-mode-hook 'custom-csharp-mode-setup t)
+
 (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13)
       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
@@ -13,10 +44,30 @@
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 )
 
+(use-package! org-super-agenda
+    :after org-agenda
+    :init
+    (setq org-super-agenda-groups '((:name "Today"
+                                        :time-grid t
+                                        :scheduled today)
+                                    (:name "Due today"
+                                        :deadline today)
+                                    (:name "Important"
+                                        :priority "A")
+                                    (:name "Due soon"
+                                        :deadline future)
+                                    (:name "Big Outcomes"
+                                        :tag "bo")
+                                    ))
+    :config
+    (org-super-agenda-mode)
+)
+
 (setq display-line-numbers-type 't)
 (global-set-key "\C-x\ t" 'toggle-truncate-lines)
+:w
 
-(setq neo-window-fixed-size nil)
+(setq projectile-project-search-path '("~/code/"))
 
 (defun prefer-horizontal-split ()
   (set-variable 'split-height-threshold nil t)
