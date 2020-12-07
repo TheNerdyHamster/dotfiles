e37abb223c4dab3d27b92dbdf3a4c7d16fb96dff
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-12-07 15:49:55 +0100

    Splited up .zshrc into other files

---
+++ .zsh/.exports.zsh
@@ -0,0 +1,13 @@
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
+
+GPG_TTY=`tty`
+export GPG_TTY
+
+export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='mvim'
+fi
