ab12186662d046bfb4af84414f130f0ec202bf06
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2020-12-09 20:40:01 +0100

    Added asdf installation

--- roles/zsh/files/zsh/.completion.zsh
+++ roles/zsh/files/zsh/.completion.zsh
@@ -1,3 +1,10 @@
+
+# append completions to fpath
+fpath=(${ASDF_DIR}/completions $fpath)
+
+# initialise completions with ZSH's compinit
+autoload -Uz compinit
+
 compinit -d ~/.cache/zsh/zcompdump-$ZSH_VERSION
 # Autocompletion for dotnet-cli
 #_dotnet_zsh_complete()
