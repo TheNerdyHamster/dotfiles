5dc391a46d2df5ad1fb6f1dc59021e4b60ae3c3a
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2020-12-11 15:00:10 +0100

    Added what_i_got alias, and cleanup+fzf

--- roles/zsh/files/zsh/.aliases.zsh
+++ roles/zsh/files/zsh/.aliases.zsh
@@ -2,3 +2,5 @@ alias ls="ls -A --color=auto"
 alias myip="curl http://ipecho.net/plain; echo"
 alias connjabra="bluetoothctl connect 70:BF:92:D1:07:D3"
 alias discjabra="bluetoothctl disconnect 70:BF:92:D1:07:D3"
+
+alias what_i_got="pacman -Qqe | fzf"
