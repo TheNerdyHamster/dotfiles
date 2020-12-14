4adfbb3ae8faecfd955e6d9d8db27ed4e9e067ef
TheNerdyHamster <leo@letnh.com>
2020-12-14 21:29:29 +0100

    Added alias for freq used commands

--- roles/zsh/files/zsh/.aliases.zsh
+++ roles/zsh/files/zsh/.aliases.zsh
@@ -1,6 +1,24 @@
+# Base
+alias bc="bc -l"
+alias c="clear"
+alias cd="cd & ls"
 alias ls="ls -A --color=auto"
+
+# Editor
+alias vi=nvim
+alias vim=nvim
+alias edit=nvim
+alias vis='nvim "+set si"'
+
+
+
+# Information
 alias myip="curl http://ipecho.net/plain; echo"
+alias ports="netstat -tulanp"
+
+# Utils
 alias connjabra="bluetoothctl connect 70:BF:92:D1:07:D3"
 alias discjabra="bluetoothctl disconnect 70:BF:92:D1:07:D3"
-
 alias what_i_got="pacman -Qqe | fzf"
+
+alias ping="ping -c 5"
