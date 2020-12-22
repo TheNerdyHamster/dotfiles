142c5cb3c39fecaa767d95320292fc524471c11b
TheNerdyHamster <leo@letnh.com>
2020-12-22 01:10:08 +0100

    Added custom wallpaper directory

--- roles/zsh/files/zsh/aliases.zsh
+++ roles/zsh/files/zsh/aliases.zsh
@@ -12,6 +12,7 @@ alias vis='nvim "+set si"'
 
 # Shortcuts
 alias dfiles="cd ~/Projects/dotfiles"
+
 # Information
 alias myip="curl http://ipecho.net/plain; echo"
 alias ports="netstat -tulanp"
@@ -20,7 +21,8 @@ alias ports="netstat -tulanp"
 alias szsh='source ~/.zshrc'
 alias connjabra="bluetoothctl connect 70:BF:92:D1:07:D3"
 alias discjabra="bluetoothctl disconnect 70:BF:92:D1:07:D3"
-alias cpic="feh --bg-fill --randomize /usr/share/backgrounds/archlinux"
+alias cpic="feh --bg-fill --randomize ~/.config/wallpapers"
+alias ppd='feh -g 640x480 -d -S filename'
 alias what_i_got="pacman -Qqe | fzf"
 
 alias ping="ping -c 5"
